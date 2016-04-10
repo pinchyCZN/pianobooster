31e4104a7d6f027b530caae442c8a4ec25767ca1
pinchyCZN <pinchyCZN@gmail.com>
2016-04-09 22:02:15 -0500

    updates

--- win32/pianolearn/midi_file.c
+++ win32/pianolearn/midi_file.c
@@ -21,11 +21,34 @@ int get_data(FILE *f,char *out,int out_size,int len)
 		memset(out,0,out_size);
 		if(len>out_size)
 			len=out_size;
-		result=fread(out,len,1,f);
+		result=fread(out,1,len,f);
 	}
 	return result;
 }
-int decode_header(FILE *f)
+int get_vlq(FILE *f,int *len)
+{
+	unsigned char buf[4];
+	int result=0,count=0,tmp;
+	do{
+		buf[0]=0;
+		if(0==fread(buf,1,1,f))
+			if(0==count)
+				return 0;
+		count++;
+		tmp=buf[0];
+		result<<=7;
+		result=tmp&0x7F;
+		if(tmp&0x80){
+			;
+		}else{
+			break;
+		}
+	}while(count<4);
+	if(len!=0)
+		*len=result;
+	return count;
+}
+int decode_header(FILE *f,int len)
 {
 	int i,val,pos;
 	char buf[8];
@@ -39,32 +62,116 @@ int decode_header(FILE *f)
 	fseek(f,pos,SEEK_SET);
 	return 0;
 }
-int decode_track(FILE *f)
+int decode_midi_event(FILE *f,int event)
 {
-
+	unsigned char buf[4]={0};
+	switch(event&0xF0){
+	case 0x80:
+	case 0x90:
+	case 0xA0:
+	case 0xB0:
+	case 0xE0:
+		{
+			int d1,d2;
+			get_data(f,buf,sizeof(buf),2);
+			d1=buf[0];
+			d2=buf[1];
+			printf("midi %02X %02X %02X\n",event,d1,d2);
+		}
+		break;
+	case 0xC0:
+	case 0xD0:
+		{
+			int d1;
+			get_data(f,buf,sizeof(buf),1);
+			d1=buf[0];
+			printf("midi %02X %02X\n",event,d1);
+		}
+		break;
+	}
 }
+int decode_track(FILE *f,int tlen)
+{
+	int index;
+	unsigned char buf[8];
+	for(index=0;index<tlen; ){
+		int delta=0;
+		int event=0;
+		int count;
+		count=get_vlq(f,&delta);
+		if(0==count)
+			break;
+		index+=count;
+		buf[0]=0;
+		if(0==fread(buf,1,1,f))
+			break;
+		index++;
+		event=buf[0];
+		switch(event){
+		case 0xF0:
+		case 0xF7:
+			//sysex events
+			{
+				int len=0;
+				count=get_vlq(f,&len);
+				if(count)
+					fseek(f,len,SEEK_CUR);
+				index+=count+len;
+				printf("sysex event %02X\n",event);
+			}
+			break;
+		case 0xFF:
+			{
+				int type,len=0;
+				get_data(f,buf,sizeof(buf),1);
+				index++;
+				type=buf[0];
+				count=get_vlq(f,&len);
+				if(count)
+					fseek(f,len,SEEK_CUR);
+				index+=count+len;
+				printf("meta event %02X len=%i\n",type,len);
+				if(type==0x2F)
+					printf("end of track\n");
+			}
+			break;
+		default: //midi event
+			{
+				printf("delta = %i ",delta);
+				decode_midi_event(f,event);
+				index+=2;
+			}
+			break;
+		}
+	}
+	return index;
+}
+
 int parse_midi_file(char *fname)
 {
 	FILE *f;
+	set_screen_buffer_size(80,9000);
 	f=fopen(fname,"rb");
 	if(f){
 		while(1){
 			char name[8],buf[8];
-			int len;
+			int len,pos;
 			if(0==get_data(f,name,sizeof(name),4))
 				break;
 			name[sizeof(name)-1]=0;
-			get_data(f,buf,sizeof(buf),4);
+			if(0==get_data(f,buf,sizeof(buf),4))
+				break;
 			len=get_int32(buf);
+			pos=ftell(f);
 
 			if(0==strcmp("MThd",name)){
-				printf("header\n");
-				decode_header(f);
+				printf("header len=%i\n",len);
+				decode_header(f,len);
 			}else if(0==strcmp("MTrk",name)){
-				printf("track\n");
+				printf("track len=%i\n",len);
+				decode_track(f,len);
 			}
-			printf("len=%i\n",len);
-			fseek(f,len,SEEK_CUR);
+			fseek(f,pos+len,SEEK_SET);
 
 		}
 		fclose(f);
--- win32/pianolearn/pianolearn.c
+++ win32/pianolearn/pianolearn.c
@@ -2,35 +2,105 @@
 #include <conio.h>
 #include <stdio.h>
 
+typedef struct{
+	char name[MAXPNAMELEN];
+	int id;
+}MIDI_DEVICE;
+
+MIDI_DEVICE *midi_dev_in=0;
+MIDI_DEVICE *midi_dev_out=0;
+int midi_dev_in_cnt=0;
+int midi_dev_out_cnt=0;
+HMIDIOUT	hmo=0;
+HMIDIIN		hmi=0;
+
+int set_screen_buffer_size(int x,int y)
+{
+	HANDLE hcon;
+	COORD size;
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	size.X=x;
+	size.Y=y;
+	SetConsoleScreenBufferSize(hcon,size);
+	return TRUE;
+}
+
 int list_midi_in()
 {
 	int i,count;
-	count=midiInGetNumDevs();
+	midi_dev_in_cnt=count=midiInGetNumDevs();
+	if(midi_dev_in!=0)
+		free(midi_dev_in);
+	midi_dev_in=0;
+	if(count==0)
+		return count;
+	midi_dev_in=calloc(count,sizeof(MIDI_DEVICE));
 	for(i=0;i<count;i++){
 		MIDIINCAPS caps={0};
-		if(MMSYSERR_NOERROR==midiInGetDevCaps(i,&caps,sizeof(caps)))
+		if(MMSYSERR_NOERROR==midiInGetDevCaps(i,&caps,sizeof(caps))){
+			if(midi_dev_in!=0){
+				strncpy(midi_dev_in[i].name,caps.szPname,sizeof(midi_dev_in[i].name));
+				midi_dev_in[i].name[sizeof(midi_dev_in[i].name)-1]=0;
+				midi_dev_in[i].id=i;
+			}
 			printf("%s\n",caps.szPname);
+		}
 	}
 	return count;
 }
 int list_midi_out()
 {
 	int i,count;
-	count=midiOutGetNumDevs();
+	midi_dev_out_cnt=count=midiOutGetNumDevs();
+	if(midi_dev_out!=0)
+		free(midi_dev_out);
+	midi_dev_out=0;
+	if(count==0)
+		return count;
+	midi_dev_out=calloc(count,sizeof(MIDI_DEVICE));
 	for(i=0;i<count;i++){
 		MIDIOUTCAPS caps={0};
-		if(MMSYSERR_NOERROR==midiOutGetDevCaps(i,&caps,sizeof(caps)))
+		if(MMSYSERR_NOERROR==midiOutGetDevCaps(i,&caps,sizeof(caps))){
+			if(midi_dev_out!=0){
+				strncpy(midi_dev_out[i].name,caps.szPname,sizeof(midi_dev_out[i].name));
+				midi_dev_out[i].name[sizeof(midi_dev_out[i].name)-1]=0;
+				midi_dev_out[i].id=i;
+			}
 			printf("%s\n",caps.szPname);
+		}
 	}
 	return count;
 }
 
+int test_midi()
+{
+	if(midi_dev_out_cnt>1){
+		hmo=0;
+		midiOutOpen(&hmo,midi_dev_out[1].id,NULL,0,CALLBACK_NULL);
+		if(hmo!=0){
+			DWORD msg;
+			char *p=&msg;
+			p[0]=0x90;
+			p[1]=0x3c;
+			p[2]=0x40;
+			midiOutShortMsg(hmo,msg);
+			Sleep(1000);
+			p[0]=0x80;
+			p[1]=0x3c;
+			p[2]=0x40;
+			midiOutShortMsg(hmo,msg);
+		}
+	}
+}
 int main(int argc,char **argv)
 {
-
-	list_midi_in();
-	list_midi_out();
-	parse_midi_file("C:\\temp\\PMLP336239-Scarlatti_Sonate_K.517.mid");
+	char *fname;
+//	list_midi_in();
+//	list_midi_out();
+//	test_midi();
+	fname="E:\\music\\Mp3\\MusicStudy\\keyboard\\Scarlatti_Sonate_K.517.mid";
+	//fname="C:\\temp\\PMLP336239-Scarlatti_Sonate_K.517.mid";
+	parse_midi_file(fname);
 	printf("done\n");
 	getch();
 }
